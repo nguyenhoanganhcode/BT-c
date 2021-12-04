3e0a4f4885710200f9cbcf3ac0c2633ac2d1ff2a
HoangAnh <nguyehoanganh8976@gmail.com>
2021-12-04 17:39:55 +0700

    BT1

---
+++ lab2/program1/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace App
+{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+--------------+\n");
+            Console.WriteLine("+-----Menu-----+\n");
+            Console.WriteLine("+--------------+\n");
+            Console.WriteLine("1.Menu 1");
+            Console.WriteLine("2.Menu 2");
+            Console.WriteLine("3.Menu 3");
+            Console.WriteLine("4.Menu 4");
+            Console.WriteLine("5.Exit");
+            
+
+
+
+
+        }
+    }
+}
